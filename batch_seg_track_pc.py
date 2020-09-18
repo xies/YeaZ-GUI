5c395bf5a3eccec251d60d045fd4b6f68052775b
Shicong Xie <xies@users.noreply.github.com>
2020-09-18 12:32:17 -0700

    added experiment directory looping

--- batch_seg_track_pc.py
+++ batch_seg_track_pc.py
@@ -9,6 +9,7 @@ Created on Wed Sep 16 14:26:32 2020
 # Import utilities
 import sys
 import numpy as np
+import pandas as pd
 from os import path
 from skimage import exposure,io
 from glob import glob
@@ -18,8 +19,8 @@ import h5py
 
 # Load CNN stuff
 # Assume script is laucnhed within YeaZ-GUI
-sys.path.append('./disk')
-sys.path.append('./unet')
+sys.path.append('/Users/xies/Desktop/Code/YeaZ-GUI/disk')
+sys.path.append('/Users/xies/Desktop/Code/YeaZ-GUI/unet')
 import Reader as nd # nd refers to nikon data for legacy reasons
 import neural_network as nn
 from segment import segment
@@ -30,10 +31,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 #%%
 # Enter directory name
-dirname = '/Users/xies/Desktop/test/'
+dirnames = ['/Users/xies/Desktop/test/']
 
 # Enter subfolder name(s) to iterate through
-subdirectories = ['pos1','pos2','pos3']
+subdirectories = ['full','pos2','pos3']
 
 # Enter the specific Channel to pick
 channel = 'Phase'
@@ -44,11 +45,15 @@ channel = 'Phase'
 # Assume: there is only one occurence of _tXXX. in the filename for parsing out what the first frame in movie is
 #         (useful for subsetting movies)
 
-tiflist = {}
-first_frame_in_folder = {}
-for subdir in subdirectories:
-    flist = sorted(glob(path.join(dirname,subdir,channel,'*.tif')))
-    tiflist[subdir] = flist
+dirlist = {}
+#NB: Opted to use nested dicts since pandas indexing becomes cumbersome for experiments w
+# different number of time frames
+for dirname in dirnames:
+    tiflist = {}
+    for subdir in subdirectories:
+        flist = sorted(glob(path.join(dirname,subdir,channel,'*.tif')))
+        tiflist[subdir] = flist
+    dirlist[dirname] = tiflist
 
     # Parse the filename to see which frame is the first file
 #    framestr = findall('_t[0-9]+.',flist[0])[0]
@@ -60,39 +65,40 @@ for subdir in subdirectories:
 mask_th = 0.3 # Threshold to generate initial mask
 seg_distance = int(3) # Min distance for watershed seeding (has to be int bc this is pixel count)
 
-for (subdir,flist) in tiflist.items():
-    
-    print('--------')
-    print(f'Now working on folder: {subdir}')
-    
-    # Initialize outputs using their h5 handler
-    outputstr = path.join(dirname,subdir,channel)
-    hdfpathstr = '' # empty string for no pre-existing data
-    newhdfstr = subdir # name of h5 file will be position folder name
-    
-    reader = nd.Reader(hdfpathstr,newhdfstr,outputstr)
-    
-    for i,f in enumerate(flist):
+for (dirname,tiflist) in dirlist.items():
+    for (subdir,flist) in tiflist.items():
         
-        # Read and pretreat the image
-        im_raw = io.imread(f)
-        im_adj = exposure.equalize_adapthist(im_raw) #Run CLAHE
-        im = im_raw.astype(np.float) # cast into float
+        print('--------')
+        print(f'Now working on folder: {subdir}')
         
-        pred = nn.prediction(im, is_pc = True)
+        # Initialize outputs using their h5 handler
+        outputstr = path.join(dirname,subdir,channel)
+        hdfpathstr = '' # empty string for no pre-existing data
+        newhdfstr = subdir # name of h5 file will be position folder name
         
-        # Threshold prediction to obtain mask
-        threshed_mask = nn.threshold(pred, mask_th)
-        # Watershed on seeds obtained from mask
-        seg = segment(threshed_mask, pred)
-        print(f'Segmented file {f}')
-        # Save to h5
-        reader.SaveMask(i,0, seg)
-        temp_mask = reader.CellCorrespondence(i, 0)
-        reader.SaveMask(i,0, temp_mask)
-        print(f'Tracked file {f}')
+        reader = nd.Reader(hdfpathstr,newhdfstr,outputstr)
         
-        
+        for i,f in enumerate(flist):
+            
+            # Read and pretreat the image
+            im_raw = io.imread(f)
+            im_adj = exposure.equalize_adapthist(im_raw) #Run CLAHE
+            im = im_raw.astype(np.float) # cast into float
+            
+            pred = nn.prediction(im, is_pc = True)
+            
+            # Threshold prediction to obtain mask
+            threshed_mask = nn.threshold(pred, mask_th)
+            # Watershed on seeds obtained from mask
+            seg = segment(threshed_mask, pred)
+            print(f'Segmented file {f}')
+            # Save to h5
+            reader.SaveMask(i,0, seg)
+            temp_mask = reader.CellCorrespondence(i, 0)
+            reader.SaveMask(i,0, temp_mask)
+            print(f'Tracked file {f}')
+            
+            
         
         
         
